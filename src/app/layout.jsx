e7d93dee1727ae53976d2ea97e3d4a941fd264f9
Song <thdrjsgml3@gmail.com>
2026-05-26 19:58:12 +0900

    폰트 CDN 수정

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,9 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Pretendard:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css" />
       </head>
       <body>{children}</body>
     </html>
